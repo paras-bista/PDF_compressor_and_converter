8e2ac35f7118fd920d1791f25ca31584bf8a24be
Parash Bista <parasbista18@gmail.com>
2025-10-17 09:56:22 +0545

    Switch database configuration to use dj_database_url

--- pdfcompressor/settings.py
+++ pdfcompressor/settings.py
@@ -69,12 +69,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'pdfcompressor.wsgi.application'
 
 # Database (SQLite for now)
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
