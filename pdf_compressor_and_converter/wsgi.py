ea462ba7693dceb3b3f9f961a6d2ae934d8b6f7e
Parash Bista <parasbista18@gmail.com>
2025-10-21 17:49:24 +0545

    Update wsgi.py

--- pdf_compressor_and_converter/wsgi.py
+++ pdf_compressor_and_converter/wsgi.py
@@ -1,16 +1,6 @@
-"""
-WSGI config for pdfcompressor project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
+# pdf_compressor_and_converter/wsgi.py
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pdfcompressor.settings')
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pdf_compressor_and_converter.settings")
 application = get_wsgi_application()
